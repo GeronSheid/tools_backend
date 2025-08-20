be25ad775fcb88b709c1688a6425aa0df618c79c
Dmitry Shneider <dmitryshneider@gmail.com>
2025-08-20 15:47:14 +0600

    index file updated

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import toolsRouter from './modules/tools/tools.router';
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+
 app.use('/api/user/', userRouter);
 app.use('/api/tools/', toolsRouter);
 
