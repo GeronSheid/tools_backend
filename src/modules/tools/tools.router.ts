1307e5654926df95fca4dd5cee54c24a04400150
Dmitry Shneider <dmitryshneider@gmail.com>
2025-08-21 23:34:29 +0600

    protected tools post, put, delete routes

--- src/modules/tools/tools.router.ts
+++ src/modules/tools/tools.router.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import { toolsController } from './tools.controller';
 import { validateBody } from '../../middlewares/validateBody';
 import { CreateToolSchema, UpdateToolSchema } from './tools.schema';
+import { authMiddleware } from '../security/auth.middleware';
 
 const toolsRouter = express.Router();
 
 toolsRouter.get("/", toolsController.getAllTools);
 toolsRouter.get("/:type", toolsController.getTypeTools);
 toolsRouter.get("/tool/:id", toolsController.getToolById);
-toolsRouter.post("/", validateBody(CreateToolSchema), toolsController.createTool);
-toolsRouter.put("/tool/:id", validateBody(UpdateToolSchema), toolsController.updateTool);
-toolsRouter.delete("/tool/:id", toolsController.deleteTool);
+toolsRouter.post("/", authMiddleware, validateBody(CreateToolSchema), toolsController.createTool);
+toolsRouter.put("/tool/:id", authMiddleware, validateBody(UpdateToolSchema), toolsController.updateTool);
+toolsRouter.delete("/tool/:id", authMiddleware, toolsController.deleteTool);
 
 export default toolsRouter;
