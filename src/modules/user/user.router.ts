01c20b717034be1386e017516d2d31a96a3ca7c8
Dmitry Shneider <dmitryshneider@gmail.com>
2025-08-22 09:45:52 +0600

    test for User Schemas ready

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -2,14 +2,13 @@ import express from 'express';
 import { userController } from './user.controler';
 import { CreateUserSchema, UpdateUserSchema } from './user.schema';
 import { validateBody } from '../../middlewares/validateBody';
-import { authController } from '../auth/auth.controller';
 import { authMiddleware } from '../security/auth.middleware';
 
 const userRouter = express.Router();
 
 userRouter.get('/', authMiddleware, userController.getAllUsers);
 userRouter.get('/:id', authMiddleware, userController.getUser);
-userRouter.post('/', authMiddleware, validateBody(CreateUserSchema), authController.register);
+userRouter.post('/', authMiddleware, validateBody(CreateUserSchema), userController.createUser);
 userRouter.put('/:id', authMiddleware, validateBody(UpdateUserSchema),  userController.updateUser);
 userRouter.delete('/:id', authMiddleware, userController.deleteUser);
 
--- src/test/unit/modules/user/user.schema.test.ts
+++ src/test/unit/modules/user/user.schema.test.ts
@@ -17,5 +17,45 @@ describe("User schema tests", () => {
         password: 'goodPassword'
     };
     expect(() => CreateUserSchema.parse(validInput)).toThrow(ZodError);
+  }),
+  it("Should validate correct password", () => {
+    const validInput = {
+        email: "valid123@gmail.com",
+        name: 'Name',
+        password: 'goodPassword'
+    };
+    expect(() => CreateUserSchema.parse(validInput)).not.toThrow();
+  }),
+  it("Should invalidate short password", () => {
+    const validInput = {
+        email: "valid123@gmail.com",
+        name: 'Name',
+        password: ''
+    };
+    expect(() => CreateUserSchema.parse(validInput)).toThrow(ZodError);
+  }),
+  it("Should invalidate long password", () => {
+    const validInput = {
+        email: "valid123@gmail.com",
+        name: 'Name',
+        password: 'veryVeryVeryVeryLongPassword'
+    };
+    expect(() => CreateUserSchema.parse(validInput)).toThrow(ZodError);
+  }),
+   it("Should invalidate short name", () => {
+    const validInput = {
+        email: "valid123@gmail.com",
+        name: '',
+        password: '12345'
+    };
+    expect(() => CreateUserSchema.parse(validInput)).toThrow(ZodError);
+  }),
+  it("Should invalidate long name", () => {
+    const validInput = {
+        email: "valid123@gmail.com",
+        name: 'VeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongName',
+        password: '12345'
+    };
+    expect(() => CreateUserSchema.parse(validInput)).toThrow(ZodError);
   })
 });
